4769bbede180d83d3dc51e9ac4ae6ef05539dc07
eddysims <eddy.s@getjobber.com>
2020-12-02 10:05:55 -0700

    day 2 - part 2

--- day2/day2.js
+++ day2/day2.js
@@ -31,9 +31,44 @@ function debugPasswords(data) {
   return result;
 }
 
-// Part 1 anser
+function charAtPos(data) {
+  const result = {
+    correct: 0,
+    incorrect: 0,
+  };
+  /**
+   * Convert a single string into an array
+   */
+  const isDataAnArray = Array.isArray(data);
+  if (!isDataAnArray) data = Array.of(data);
+
+  data.forEach((item) => {
+    /**
+     * Split the string into things we care about
+     */
+    const [pos1, pos2, matcher, password] = item.split(/(?:-| |:)+/);
+
+    if (password[pos1 - 1] === matcher && password[pos2 - 1] === matcher) {
+      result.incorrect++;
+    } else if (
+      password[pos1 - 1] === matcher ||
+      password[pos2 - 1] === matcher
+    ) {
+      result.correct++;
+    } else {
+      result.incorrect++;
+    }
+  });
+
+  return result;
+}
+
+// Part 1 answer
 // console.log(debugPasswords(data));
+// Part 2 answer
+// console.log(charAtPos(data));
 
 module.exports = {
   debugPasswords,
+  charAtPos,
 };
--- day2/day2.test.js
+++ day2/day2.test.js
@@ -1,20 +1,41 @@
-const { expect } = require("@jest/globals");
-const { debugPasswords } = require("./day2");
+const { debugPasswords, charAtPos } = require("./day2");
 
 const mock = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
 
-it("should return correct for 1st mock", () => {
-  const passwords = debugPasswords(mock[0]);
-  expect(passwords.correct).toBe(1);
-});
+describe('debugPasswords function', () => {
+  it("should return correct for 1st mock", () => {
+    const passwords = debugPasswords(mock[0]);
+    expect(passwords.correct).toBe(1);
+  });
 
-it("should return incorrect for 2st mock", () => {
-  const passwords = debugPasswords(mock[1]);
-  expect(passwords.incorrect).toBe(1);
-});
+  it("should return incorrect for 2st mock", () => {
+    const passwords = debugPasswords(mock[1]);
+    expect(passwords.incorrect).toBe(1);
+  });
 
-it.only("should return correct 2 and incorrect 1 for mock", () => {
-  const {correct, incorrect} = debugPasswords(mock);
-  expect(correct).toBe(2);
-  expect(incorrect).toBe(1);
+  it("should return correct 2 and incorrect 1 for mock", () => {
+    const { correct, incorrect } = debugPasswords(mock);
+    expect(correct).toBe(2);
+    expect(incorrect).toBe(1);
+  });
+});
+ 
+describe('charPos function', () => {
+  test("should return correct for 1st mock", () => {
+    const { correct, incorrect } = charAtPos(mock[0]);
+    expect(correct).toBe(1);
+    expect(incorrect).toBe(0);
+  });
+
+  test("should return incorrect for 2st mock", () => {
+    const { correct, incorrect } = charAtPos(mock[1]);
+    expect(correct).toBe(0);
+    expect(incorrect).toBe(1);
+  });
+
+  test("should return correct 1 and incorrect 2 for mock", () => {
+    const { correct, incorrect } = charAtPos(mock);
+    expect(correct).toBe(1);
+    expect(incorrect).toBe(2);
+  });
 });
